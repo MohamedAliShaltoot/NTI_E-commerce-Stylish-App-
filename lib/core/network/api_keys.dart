c0350500db73f49534f777db1052f21015b99949
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-05-17 11:49:46 +0300

    add all conts apiKets

--- lib/core/network/api_keys.dart
+++ lib/core/network/api_keys.dart
@@ -1,8 +1,21 @@
 abstract class ApiKeys {
   static const String userNameApiKey = 'username';
+  static const String nameApiKey = 'name';
+
   static const String passwordApiKey = 'password';
+  static const String emailApiKey = 'email';
+  static const String phoneApiKey = 'phone';
   static const String imageApiKey = 'image';
-  static const String currentPasswordKey = "current_password";
+
+  static const String descriptionApiKey = "description";
+  static const String titleApiKey = "title";
+  static const String priceApiKey = "price";
+  static const String bestSellerApiKey = "best_seller";
+  static const String ratingApiKey = "rating";
+  static const String productIdApiKey = "product_id";
+  static const String paramsApiKey = "q";
+
+  static const String categoryIDApiKey = "category_id";
   static const String newPasswordKey = "new_password";
   static const String newPasswordConfirmKey = "new_password_confirm";
 }
