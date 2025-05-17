e35c85492d116face9312e8e5aaca80b81aef25b
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-05-17 15:33:45 +0300

    add new assets paths

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -11,7 +11,19 @@ abstract class AppAssets {
   static const String onboarding_3 = '${_basePath}onboarding_3.svg';
   static const String phoneIcon = '${_basePath}phone_icon.svg';
   static const String userIcon = '${_basePath}user_icon.svg';
-   static const String leftIcon = '${_basePath}icon_left.svg';
-    static const String rightIcon = '${_basePath}icon_right.svg';
-    static const String eyeIcon = '${_basePath}eye_icon.svg';
+  static const String leftIcon = '${_basePath}icon_left.svg';
+  static const String rightIcon = '${_basePath}icon_right.svg';
+  static const String eyeIcon = '${_basePath}eye_icon.svg';
+
+  static const String bagIcon = '${_basePath}bag_icon.svg';
+  static const String bagBoldIcon = '${_basePath}bag_bold_icon.svg';
+  static const String dotIcon = '${_basePath}dot_image_profile.png';
+
+  static const String heartIcon = '${_basePath}heart_icon.svg';
+  static const String homeIcon = '${_basePath}home.svg';
+  static const String logOutIcon = '${_basePath}log_out_icon.svg';
+  static const String profileIcon = '${_basePath}profile_icon.svg';
+  static const String profileImage = '${_basePath}profile_image.png';
+  static const String shoppingCartIcon = '${_basePath}shopping-cart.svg';
+  static const String settingsIcon = '${_basePath}setting.svg';
 }
