14e55a79382503ccc475caa29c05743d0fc10110
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-05-17 11:50:06 +0300

    add all endPoints

--- lib/core/network/end_points.dart
+++ lib/core/network/end_points.dart
@@ -1,26 +1,46 @@
-abstract class EndPoints
-{
-  static const String baseUrl = 'https://ntitodo-production-e01d.up.railway.app/api/';
+abstract class EndPoints {
+  static const String baseUrl =
+      'https://ntitodo-production-e01d.up.railway.app/api/';
 
+  //user
   static const String register = 'register';
-   static const String login = 'login';
-   static const String getUserData = 'get_user_data';
-   static const String addTask = 'new_task';
-    static const String getTasks = 'my_tasks';
-    static const String updateUserProfile = 'update_profile';
-    static const String getUser = 'delete_user';
-    static const String changePassword = 'change_password';
-    static const String refreshToken = 'refresh_token';
-    static const String updateTask = 'tasks/';
-    static const String deleteTask = 'tasks/';
-//static const String refreshToken = 'refresh_token';
-    // static const String getTasks = 'my_tasks';
-    // static const String getTasks = 'my_tasks';
-  
+  static const String login = 'login';
+  static const String getUserData = 'get_user_data';
+  static const String updateUserProfile = 'update_profile';
+  static const String getUser = 'delete_user';
+  static const String refreshToken = 'refresh_token';
+
+  //sliders
+
+  static const String newSlider = 'new_slider';
+  static const String editSlider = 'slider/';
+  static const String deleteSlider = 'slider/';
+  static const String getSliders = 'sliders';
+
+  //categories
+  static const String newCategory = 'new_category';
+  static const String editCategory = 'category/';
+  static const String deleteCategory = 'category/';
+  static const String getCategories = 'categories';
+
+  //orders
+  static const String placeOrder = 'place_order';
+  static const String cancelOrder = 'orders/cancel/';
+  static const String completeOrder = 'orders/complete/';
+  static const String getOrders = 'orders';
+
+  //products
+  static const String newProduct = 'new_product';
+  static const String addToFavorite = 'add_to_favorite';
+  static const String editProduct = 'product/';
+  static const String deleteProduct = 'product/';
+  static const String getProducts = 'products';
+  static const String getProductByCategory = 'products/search?q=p';
+  static const String bestSellerProduct = 'best_seller_products';
+  static const String topRatedProduct = 'top_rated_products';
 }
 
-// username : MOhamedgg
-// password :mohamedalishaltoot
+
 
 
 
