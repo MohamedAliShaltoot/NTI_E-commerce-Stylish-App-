59a781ccbf2b7fd7e0e2ae80d07e6e520ef0763c
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2025-05-17 15:33:19 +0300

    make svg icon to take color

--- lib/core/helper/custom_svg_picture.dart
+++ lib/core/helper/custom_svg_picture.dart
@@ -2,17 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class CustomSvg extends StatelessWidget {
-   const CustomSvg({super.key, required this.path, this.height, this.width, this.onTap,});
+   const CustomSvg({super.key, required this.path, this.height, this.width, this.onTap, this.color,});
 
   final String path;
   final double? height;
   final double? width;
  final void Function()? onTap;
+ final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap ,
-      child: SvgPicture.asset(path, height: height, width: width));
+      child: SvgPicture.asset(path, height: height, width: width,
+        color: color,
+       // fit: BoxFit.cover,
+      ),);
   }
 }
